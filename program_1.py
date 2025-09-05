40fcd84939009e10075394414e402d13b91bb336
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-09-05 10:55:15 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,8 @@
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    # programer Devin Goshaw
+    # Date 9/4/2025
+    print('Hello World')
 
 hello_world()
